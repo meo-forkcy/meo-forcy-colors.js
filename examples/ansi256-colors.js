b2474ea088c7a6e4ba76d182a05979d05dd9a231
meo-forkcy <meoclonemeoclone@gmail.com>
2025-05-14 23:35:53 +0700

    Added test for gradient

--- examples/ansi256-colors.js
+++ examples/ansi256-colors.js
@@ -1,10 +1,12 @@
+var colors = require("../lib/index");
+
 function printANSI256ColorTable() {
-  console.log('\n🎨 ANSI 256 Color Table:\n');
+  console.log("\n🎨 ANSI 256 Color Table:\n");
 
   for (let i = 0; i < 256; i++) {
     const bg = `\x1b[48;5;${i}m`;
     const reset = `\x1b[0m`;
-    const label = i.toString().padStart(3, ' ');
+    const label = i.toString().padStart(3, " ");
     process.stdout.write(`${bg} ${label} ${reset} `);
 
     if ((i + 1) % 16 === 0) console.log(); // Newline every 16 colors
@@ -12,3 +14,6 @@ function printANSI256ColorTable() {
 }
 
 printANSI256ColorTable();
+
+// gradient test
+console.log(colors.gradient("This is gradient string!"));

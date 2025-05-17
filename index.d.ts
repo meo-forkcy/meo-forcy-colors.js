d6eff9a04d27c6cb05616a2d54f9a6fd51ed100e
meo-forkcy <meoclonemeoclone@gmail.com>
2025-05-17 23:52:54 +0700

    Added contribute

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 // Type definitions for Colors.js 1.2
-// Project: https://github.com/Marak/colors.js
-// Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Staffan Eketorp <https://github.com/staeke>
-// Definitions: https://github.com/Marak/colors.js
+// Original Project: https://github.com/Marak/colors.js
+// Original Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Staffan Eketorp <https://github.com/staeke>
+// Original Definitions: https://github.com/Marak/colors.js
+// Maintenance by meo-forkcy with love <3
 
 export interface Color {
   (text: string): string;
